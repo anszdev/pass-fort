3c3a4c652979c8a85a5d10f2ebd326061de5ed89
baezdev <baezdev@gmail.com>
2025-03-13 00:00:05 -0600

    Add root layout, fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,38 @@
-import { Stack } from 'expo-router';
+import { useThemeColors } from '@core/hooks/useThemeColors';
+import { useFonts } from 'expo-font';
+import { Stack, SplashScreen } from 'expo-router';
+import { useEffect } from 'react';
+import { Fonts } from '@core/constants/Fonts';
+/* import * as SystemUI from 'expo-system-ui'; */
 
 export default function RootLayout() {
-  return <Stack />;
+  const [loaded] = useFonts(Fonts);
+  const { colors } = useThemeColors();
+
+  /* useEffect(() => {
+    SystemUI.setBackgroundColorAsync(colors.background);
+  }, [colors.background]); */
+
+  useEffect(() => {
+    SplashScreen.preventAutoHideAsync();
+
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: colors.background },
+      }}
+    >
+      <Stack.Screen name="(auth)" />
+    </Stack>
+  );
 }
