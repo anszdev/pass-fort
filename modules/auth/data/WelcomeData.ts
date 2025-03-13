16beadd1546a5ad46b2bee690ec48c70e24f7526
baezdev <baezdev@gmail.com>
2025-03-13 00:06:39 -0600

    Add welcome component

---
+++ modules/auth/data/WelcomeData.ts
@@ -0,0 +1,20 @@
+export const WELCOME_DATA = [
+  {
+    title: 'Con Pass Fort ¡Olvídate de olvidar!',
+    content:
+      'Protege y accede a tus contraseñas en un solo lugar y con un clic. La seguridad nunca fue tan sencilla.',
+    image: require('@assets/images/welcome/welcome1.png'),
+  },
+  {
+    title: 'Sincronización en todos tus dispositivos',
+    content:
+      'Accede a tus credenciales desde cualquier lugar, en tu computadora, teléfono o tablet.',
+    image: require('@assets/images/welcome/welcome2.png'),
+  },
+  {
+    title: 'Autocompletado inteligente',
+    content:
+      'Olvídate de escribir contraseñas una y otra vez. Rellena automáticamente tus credenciales en cualquier sitio web.',
+    image: require('@assets/images/welcome/welcome3.png'),
+  },
+];
