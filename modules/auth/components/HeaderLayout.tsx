580b0d2fb72c2ecf87570bb4294c62867df41867
anszdev <anszdev@outlook.com>
2025-04-03 21:44:13 -0600

    Refactor HeaderAuthLayout to include back navigation button and adjust layout styles

--- modules/auth/components/HeaderLayout.tsx
+++ modules/auth/components/HeaderLayout.tsx
@@ -1,10 +1,27 @@
 import { StyleSheet, View } from 'react-native';
+import { useRouter } from 'expo-router';
 import { Logo } from '@core/components/Icons';
+import { ButtonIcon } from '@core/components/ButtonIcon';
 
-export function Header({ backgroundColor }: { backgroundColor: string }) {
+export function HeaderAuthLayout({
+  backgroundColor,
+  route,
+}: {
+  backgroundColor: string;
+  route?: string;
+}) {
+  const router = useRouter();
+
   return (
     <View style={[styles.header, { backgroundColor }]}>
-      <Logo />
+      {route !== 'index' && (
+        <View style={styles.buttonContainer}>
+          <ButtonIcon icon="chevron-left" onPress={() => router.back()} />
+        </View>
+      )}
+      <View style={styles.logoContainer}>
+        <Logo />
+      </View>
     </View>
   );
 }
@@ -12,8 +29,18 @@ export function Header({ backgroundColor }: { backgroundColor: string }) {
 const styles = StyleSheet.create({
   header: {
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     backgroundColor: '#F5F5F5',
     paddingVertical: 9,
+    flexDirection: 'row',
+    position: 'relative',
+  },
+  logoContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  buttonContainer: {
+    position: 'absolute',
+    left: 16,
   },
 });
