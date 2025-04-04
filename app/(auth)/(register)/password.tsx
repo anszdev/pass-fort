bbd5b77fa20d83f25b214690252c7142c2a5eec6
anszdev <anszdev@outlook.com>
2025-04-04 00:02:31 -0600

    Add password page

--- app/(auth)/(register)/password.tsx
+++ app/(auth)/(register)/password.tsx
@@ -1,5 +1,44 @@
 import { View } from 'react-native';
+import { Field } from '@core/components/Field';
+import { Button } from '@core/components/Button';
+import { FormContainer } from '@auth/components/FormContainer';
+import { FormTitle } from '@auth/components/FormTitle';
+import { Steps } from '@auth/components/Steps';
 
 export default function Password() {
-  return <View></View>;
+  const createAccount = () => {
+    console.log('contraseña confirmada');
+  };
+
+  return (
+    <FormContainer
+      formFooter={
+        <>
+          <Steps steps={3} currentStep={3} />
+          <Button
+            text="Crear Cuenta"
+            variant="primary"
+            width="fit"
+            onPress={createAccount}
+          />
+        </>
+      }
+    >
+      <FormTitle title="Crea y confirma tu contraseña" salute="Hey 🤐" />
+      <View style={{ gap: 24, marginTop: 46 }}>
+        <Field
+          label="Contraseña"
+          icon="lock"
+          textContentType="password"
+          secureTextEntry
+        />
+        <Field
+          label="Confirma la contraseña"
+          icon="lock"
+          textContentType="password"
+          secureTextEntry
+        />
+      </View>
+    </FormContainer>
+  );
 }
