29361d7e0498b2611be35e77552451b1803ba1a3
anszdev <anszdev@outlook.com>
2025-04-03 21:35:32 -0600

    Refactor authentication screens: remove AuthScreen, update Index to include Carrusel and navigation links, and add Login and ResetPassword components

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,5 +1,28 @@
-import { WelcomeScreen } from '@auth/screens/WelcomeScreen';
+import { StyleSheet, View } from 'react-native';
+import { Link } from 'expo-router';
+import { Carrusel } from '@auth/components/Carrusel';
+import { Button } from '@core/components/Button';
+import { Container } from '@core/components/Container';
 
 export default function Index() {
-  return <WelcomeScreen />;
+  return (
+    <Container>
+      <Carrusel />
+      <View style={styles.action}>
+        <Link href="/login" asChild>
+          <Button text="Iniciar sesión" />
+        </Link>
+        <Link href="/(auth)/(register)" asChild>
+          <Button text="Registrarse" variant="transparent" />
+        </Link>
+      </View>
+    </Container>
+  );
 }
+
+const styles = StyleSheet.create({
+  action: {
+    marginVertical: 24,
+    gap: 8,
+  },
+});
---
+++ app/(auth)/login.tsx
@@ -0,0 +1,28 @@
+import { View } from 'react-native';
+import { Button } from '@core/components/Button';
+import { Field } from '@core/components/Field';
+import { FormTitle } from '@auth/components/FormTitle';
+import { FormLinkBottom, FormLinkTop } from '@auth/components/FormLink';
+import { FormContainer } from '@auth/components/FormContainer';
+
+export default function Login() {
+  return (
+    <FormContainer actionContent={<Button text="Iniciar Sesión" />}>
+      <FormTitle title="Inicia sesión ahora!" salute="Hey 👋🏽" />
+      <FormLinkTop
+        href="/(auth)/(register)"
+        textLink="Registrate"
+        description="¿Aún no tienes cuenta?"
+      />
+      <View style={{ gap: 24 }}>
+        <Field label="Correo o Número de Telefono" icon="hash" />
+        <Field label="Contraseña" icon="lock" />
+      </View>
+      <FormLinkBottom
+        href="/reset-password"
+        textLink="Reseteala"
+        description="¿Olvidaste tu contraseña?"
+      />
+    </FormContainer>
+  );
+}
