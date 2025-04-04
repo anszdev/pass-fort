da0747eb2ee2370cc9d0027828ac02a0307fbbae
anszdev <anszdev@outlook.com>
2025-04-03 21:35:55 -0600

    Add Register component with form fields and navigation to OTP code

---
+++ app/(auth)/(register)/index.tsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import { View } from 'react-native';
+import { FieldCheck } from '@core/components/FieldCheck';
+import { Field } from '@core/components/Field';
+import { FormTitle } from '@auth/components/FormTitle';
+import { FormLinkTop } from '@auth/components/FormLink';
+import { FormContainer } from '@auth/components/FormContainer';
+import { Link } from 'expo-router';
+import { ButtonIcon } from '@core/components/ButtonIcon';
+
+export default function Register() {
+  const [isChecked, setIsChecked] = useState(false);
+  return (
+    <FormContainer
+      actionContent={
+        <Link href="/(auth)/(register)/otp-code" asChild>
+          <ButtonIcon
+            icon="arrow-right"
+            variant="primary"
+            disabled={!isChecked}
+          />
+        </Link>
+      }
+    >
+      <FormTitle title="Registrate ahora!" salute="Hey, bienvenid@ 👋🏽" />
+      <FormLinkTop
+        href="/login"
+        textLink="Inicia Sesión"
+        description="¿Ya tienes cuenta?"
+      />
+      <View style={{ gap: 24 }}>
+        <Field label="Correo o Número de Telefono" value="" icon="hash" />
+        <FieldCheck
+          label="Acepto los términos y condiciones"
+          toggleCheck={setIsChecked}
+          isChecked={isChecked}
+        />
+      </View>
+    </FormContainer>
+  );
+}
