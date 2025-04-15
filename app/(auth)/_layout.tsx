22e5a27aa382221f4cf7edcba374859aea53694a
anszdev <anszdev@outlook.com>
2025-04-15 00:59:26 -0600

    Wrap Stack component in KeyboardProvider for improved keyboard handling

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,25 +1,28 @@
 import { Stack } from 'expo-router';
 import { useThemeColors } from '@core/hooks/useThemeColors';
 import { HeaderAuthLayout } from '@auth/components/HeaderLayout';
+import { KeyboardProvider } from 'react-native-keyboard-controller';
 
 export default function AuthLayout() {
   const { colors } = useThemeColors();
   return (
-    <Stack
-      screenOptions={({ route }) => ({
-        headerTitle: () => null,
-        header: () => (
-          <HeaderAuthLayout
-            backgroundColor={colors.background}
-            route={route.name}
-          />
-        ),
-      })}
-    >
-      <Stack.Screen name="index" />
-      <Stack.Screen name="login" />
-      <Stack.Screen name="(register)" />
-      <Stack.Screen name="reset-password" />
-    </Stack>
+    <KeyboardProvider>
+      <Stack
+        screenOptions={({ route }: { route: any }) => ({
+          headerTitle: () => null,
+          header: () => (
+            <HeaderAuthLayout
+              backgroundColor={colors.background}
+              route={route.name}
+            />
+          ),
+        })}
+      >
+        <Stack.Screen name="index" />
+        <Stack.Screen name="login" />
+        <Stack.Screen name="(register)" />
+        <Stack.Screen name="reset-password" />
+      </Stack>
+    </KeyboardProvider>
   );
 }
