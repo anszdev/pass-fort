1a8fbb5588b53fe5f1955beff489f3a46ff61904
anszdev <anszdev@outlook.com>
2025-04-27 17:22:02 -0600

    Init server project

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+  port: 3001,
+  fetch: (req) => {
+    return new Response("Hello World");
+  },
+});
+
+console.log(`Server running at http://localhost:${server.port}`);
